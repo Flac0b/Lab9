097b78f54c019d8667d8ba2c4565c4ff2230c78c
Flac0b <alfredbrewer2004@gmail.com>
2024-04-11 14:41:48 -0400

    main add

---
+++ main.py
@@ -0,0 +1,36 @@
+def encoder(code):
+    result = ""
+
+    for x in str(code):
+        if int(x) < 7:
+            result += str(int(x) + 3)
+        else:
+            result += str((int(x) - 7))
+
+    return result
+
+def decoder(code):
+    pass
+
+if __name__ == "__main__":
+    while True:
+
+        print("""
+        Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+""")
+
+        option = input("Please enter an option: ")
+        original = ""
+
+        if option == 1:
+            stored = encoder(input("Please enter your password to encode: "))
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            print(f"The encoded password is {stored}, and the original password is {decoder(stored)}.")
+
+
+
